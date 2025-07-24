cdc55c0598e226531e6cbac0a03b4486b68e3462
vikram641 <vikram8575229@gmail.com>
2025-07-24 16:42:50 +0530

    second commit

--- src/routes/userRouts.js
+++ src/routes/userRouts.js
@@ -2,8 +2,8 @@ const express = require("express");
 const { signup, signin } = require("../controlers/userControler");
 const userRouter = express.Router();
 
-userRouter.get("/signup", signup);
+userRouter.post("/signup", signup);
 
-userRouter.get("/signin", signin);
+userRouter.post("/signin", signin);
 
 module.exports = userRouter;
